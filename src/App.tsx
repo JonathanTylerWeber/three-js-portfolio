d34cfc695c1a0e17d888f82705bfa8784ba3d80c
Jonathan Tyler Weber <jonathantweber@gmail.com>
2025-07-12 19:47:18 +0800

    leva out of canvas

--- src/App.tsx
+++ src/App.tsx
@@ -13,26 +13,28 @@ import { Leva } from "leva";
 
 export default function App() {
   return (
-    <Canvas
-      camera={{ fov: 45, near: 0.1, far: 200, position: [0, 2, 6] }}
-      dpr={[1, 1.5]}
-      gl={{ powerPreference: "high-performance" }}
-      className="touch-none"
-    >
-      <Suspense fallback={null}>
-        <Physics timeStep="vary" maxCcdSubsteps={1}>
-          <Leva collapsed />
-          {/* <PhysicsDebugger /> */}
-          <Perf position="top-left" minimal={false} deepAnalyze={false} />
+    <>
+      <Leva collapsed />
+      <Canvas
+        camera={{ fov: 45, near: 0.1, far: 200, position: [0, 2, 6] }}
+        dpr={[1, 1.5]}
+        gl={{ powerPreference: "high-performance" }}
+        className="touch-none"
+      >
+        <Suspense fallback={null}>
+          <Physics timeStep="vary" maxCcdSubsteps={1}>
+            {/* <PhysicsDebugger /> */}
+            <Perf position="top-left" minimal={false} deepAnalyze={false} />
 
-          <World />
-          <WorldColliders />
-          <Grass fieldSize={132.88} maskSize={150} bladeCount={120_000} />
-          <Bugs />
-          <CharacterController />
-          <Ball url="/models/ball.glb" radius={1} spawn={[-10, 1, -30]} />
-        </Physics>
-      </Suspense>
-    </Canvas>
+            <World />
+            <WorldColliders />
+            <Grass fieldSize={132.88} maskSize={150} bladeCount={120_000} />
+            <Bugs />
+            <CharacterController />
+            <Ball url="/models/ball.glb" radius={1} spawn={[-10, 1, -30]} />
+          </Physics>
+        </Suspense>
+      </Canvas>
+    </>
   );
 }
